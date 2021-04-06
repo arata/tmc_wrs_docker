e833a522a482f6b8cefa39e867679be0c11b50b9
arata_sakamaki <arata.sakamaki@okadanet.org>
2021-04-06 23:25:03 +0900

    change compose file and add shell

---
+++ docker-compose-up.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+FILE="$HOME/.gitconfig"
+if [ -d $FILE ]; then
+    echo ".gitconfig is directory"
+    mv $FILE $FILE.bak
+    touch $FILE
+elif [ -f $FILE ]; then
+    echo ".gitconfig is exsists and its text file"
+else
+    touch $FILE
+fi
+
+if [ "$1" = "nvidia" ]; then
+    if [ $DISPLAY = :1 ]; then
+	echo "Your DISPLAY environment is :1"
+	export DISPLAY_ENV=:0
+	export DISPLAY_PATH=X1
+    fi
+    echo "Use nvidia"
+    DISPLAY=$DISPLAY xhost si:localuser:root
+    docker-compose -f docker-compose.nvidia.yml up
+elif [ $# = 0 ]; then
+    docker-compose up
+else
+    echo "Plese set correct argument"
+    exit 1
+fi
